624899ea383ad4d269dc12c6d480ea63cd13ce2c
trevor <>
2021-02-24 12:32:34 -0500

    install minikube

--- system_setup.sh
+++ system_setup.sh
@@ -45,11 +45,15 @@ sudo ./aws/install
 # install python poetry
 curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
 
-# install kubectl
+# install kubectl and minikube
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/\
 $(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
-mv kubectl /usr/local/bin/kubectl
+sudo mv kubectl /usr/local/bin
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 
+chmod +x minikube
+sudo mv minikube /usr/local/bin
+
 
 # install helm
 curl -LO https://git.io/get_helm.sh
